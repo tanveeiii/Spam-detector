00ddc653bbd4297073d918fda3ddf957cf1d7eb6
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-21 23:33:52 +0530

    Connecting the traiinMoodel.py file with the Flask app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import trainModel
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if(request.method=="GET"):
+      return render_template("home.html")
+    elif(request.method=="POST"):
+       email_content = request.form.get('email')
+       result = trainModel.training(email_content)
+       return result
+
 
 if __name__ == "__main__":
   app.run(debug=True)
--- trainModel.py
+++ trainModel.py
@@ -7,7 +7,7 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 def load_data(data_path):
     data =[]
@@ -25,54 +25,56 @@ def load_data(data_path):
 
     return pd.DataFrame(data, columns=['text', 'label'])
 
-training_data = load_data('train-mails')
-test_data = load_data('test-mails')
+def training(email):
+    training_data = load_data('train-mails')
+    test_data = load_data('test-mails')
 
-X_train = training_data['text']
-y_train = training_data['label']
-X_test = test_data['text']
-y_test = test_data['label']
+    X_train = training_data['text']
+    y_train = training_data['label']
+    X_test = test_data['text']
+    y_test = test_data['label']
 
-tfidf_vectorizer = TfidfVectorizer(max_features=5000)
-X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-X_test_tfidf = tfidf_vectorizer.transform(X_test)
+    tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+    X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
-lgClassifier = LogisticRegression()
-lgClassifier.fit(X_train_tfidf, y_train)
-y_pred1 = lgClassifier.predict(X_test_tfidf)
-score1 = accuracy_score(y_test, y_pred1)
-cm1 = confusion_matrix(y_test, y_pred1)
-# accuracy = 0.9769230769230769
+    lgClassifier = LogisticRegression()
+    lgClassifier.fit(X_train_tfidf, y_train)
+    y_pred1 = lgClassifier.predict(X_test_tfidf)
+    score1 = accuracy_score(y_test, y_pred1)
+    cm1 = confusion_matrix(y_test, y_pred1)
+    # accuracy = 0.9769230769230769
 
-nbClassifier = GaussianNB()
-nbClassifier.fit(X_train_tfidf.toarray(), y_train.to_numpy())
-y_pred2 = nbClassifier.predict(X_test_tfidf.toarray())
-score2 = accuracy_score(y_test, y_pred2)
-cm2 = confusion_matrix(y_test, y_pred2)
-# accuracy = 0.9461538461538461
+    nbClassifier = GaussianNB()
+    nbClassifier.fit(X_train_tfidf.toarray(), y_train.to_numpy())
+    y_pred2 = nbClassifier.predict(X_test_tfidf.toarray())
+    score2 = accuracy_score(y_test, y_pred2)
+    cm2 = confusion_matrix(y_test, y_pred2)
+    # accuracy = 0.9461538461538461
 
-rfClassifier = RandomForestClassifier()
-rfClassifier.fit(X_train_tfidf, y_train)
-y_pred3 = rfClassifier.predict(X_test_tfidf)
-score3 = accuracy_score(y_test, y_pred3)
-cm3 = confusion_matrix(y_test, y_pred3)
-# accuracy = 0.9807692307692307
+    rfClassifier = RandomForestClassifier()
+    rfClassifier.fit(X_train_tfidf, y_train)
+    y_pred3 = rfClassifier.predict(X_test_tfidf)
+    score3 = accuracy_score(y_test, y_pred3)
+    cm3 = confusion_matrix(y_test, y_pred3)
+    # accuracy = 0.9807692307692307
 
-knn = KNeighborsClassifier(n_neighbors = 5)
-knn.fit(X_train_tfidf, y_train)
-y_pred4 = knn.predict(X_test_tfidf)
-score4 = accuracy_score(y_test, y_pred4)
-cm4 = confusion_matrix(y_test, y_pred4)
-# accuracy = 0.9730769230769231
+    knn = KNeighborsClassifier(n_neighbors = 5)
+    knn.fit(X_train_tfidf, y_train)
+    y_pred4 = knn.predict(X_test_tfidf)
+    score4 = accuracy_score(y_test, y_pred4)
+    cm4 = confusion_matrix(y_test, y_pred4)
+    # accuracy = 0.9730769230769231
 
-svc = SVC(kernel='linear')
-svc.fit(X_train_tfidf, y_train)
-y_pred5 = svc.predict(X_test_tfidf)
-score5 = accuracy_score(y_test, y_pred5)
-cm5 = confusion_matrix(y_test, y_pred5)
-# accuracy = 0.9846153846153847
-
-
-
-
-
+    svc = SVC(kernel='linear')
+    svc.fit(X_train_tfidf, y_train)
+    y_pred5 = svc.predict(X_test_tfidf)
+    score5 = accuracy_score(y_test, y_pred5)
+    cm5 = confusion_matrix(y_test, y_pred5)
+    # accuracy = 0.9846153846153847
+    email_converted = tfidf_vectorizer.transform([email])
+    result = svc.predict(email_converted)
+    if(result[0]==1):
+        return "spam"
+    else:
+        return "ham"
